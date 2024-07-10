b4fa63541170d3d90a429076f2d1ff01a24060dd
Brett Czerwinski <brettcz@outlook.com>
2024-07-10 15:54:50 -0600

    fix seeding bug

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Comment, Post } = require('../models');
-
-const userData = require('./userData.json');
-const commentData = require('./commentData.json');
-const postData = require('./postData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
     process.exit(0);
   };
   
-  seedDatabase();
+seedDatabase();
